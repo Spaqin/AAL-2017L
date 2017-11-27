406b11aa488a8ec381a728d541c3ebadd6cc8aa1
Spaqin <3garfield@gmail.com>
2017-11-27 23:35:27 +0100

    initial commit

---
+++ solvers/bfs.py
@@ -0,0 +1,14 @@
+from networkx import Graph
+
+
+class BFS(object):
+    def __init__(self):
+        self.visited = []
+        self.path = []
+        self.stack = []
+
+    def solve_graph(self, graph):
+        pass
+
+    def bfs_recursion(self, graph):
+        pass
---
+++ solvers/dijkstra.py
@@ -0,0 +1,9 @@
+from networkx import Graph
+
+
+class Dijkstra(object):
+    def __init__(self):
+        pass
+
+    def solve_graph(self, graph):
+        pass
