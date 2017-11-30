3b44671b43c1eb2212e9a41277e728f62a47074d
Spaqin <3garfield@gmail.com>
2017-11-30 11:37:10 +0100

    some work on solvers

--- solvers/bfs.py
+++ solvers/bfs.py
@@ -1,14 +1,40 @@
 from networkx import Graph
+import queue as q
 
 
 class BFS(object):
-    def __init__(self):
-        self.visited = []
-        self.path = []
-        self.stack = []
+    def __init__(self, graph, start, end):
+        self.graph = graph
+        self.meta = dict()
+        self.start = start
+        self.end = end
 
-    def solve_graph(self, graph):
-        pass
+    def solve_graph(self):
+        visited = set()
+        path = []
+        queue = q.Queue()
+        queue.put_nowait(self.start)
 
-    def bfs_recursion(self, graph):
-        pass
+        while not queue.empty:
+            parent = queue.get_nowait()
+            if parent == self.end:
+                return self.construct_path(parent)
+
+            for child in self.graph.neighbors(parent):
+                if child in visited:
+                    continue
+                if child not in queue:
+                    self.meta[child] = parent
+                    queue.put_nowait(child)
+
+            visited.add(parent)
+        return []
+
+    def construct_path(self, last_state):
+        path = []
+        current_node = last_state
+        while current_node != self.start:
+            path.append(current_node)
+            current_node = self.meta[current_node]
+
+        return path.reverse()
--- solvers/dijkstra.py
+++ solvers/dijkstra.py
@@ -1,9 +1,22 @@
 from networkx import Graph
 
+g = Graph
+
 
 class Dijkstra(object):
-    def __init__(self):
-        pass
+    def __init__(self, graph, start, end, distance=1):
+        self.graph = graph
+        self.meta = dict()
+        self.start = start
+        self.end = end
+        self.DISTANCE = distance  # distance between nodes is const
 
-    def solve_graph(self, graph):
-        pass
+    def solve_graph(self):
+        unvisited = list(self.graph.nodes)
+        dist = dict()
+        prev = dict()
+        for node in unvisited:
+            # distance to the end node cannot be longer than number of nodes*dist
+            dist[node] = self.graph.number_of_nodes()*self.DISTANCE + 1
+            prev[node] = None
+            # TODO everything
